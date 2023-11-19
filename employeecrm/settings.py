b1aeb23567804389c96c2d1f9b4e09f571c54aa8
parvathy <pezhathinalbineesh@gmail.com>
2023-11-19 21:16:26 +0530

    add new file

--- employeecrm/settings.py
+++ employeecrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sp(%rxye7=xv221zrdmplbpckrlyajciaiu-nz$dt)obp@_yh_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
